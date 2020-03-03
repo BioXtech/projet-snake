06337e3635c304385a6c2d6ee40276f78b287d7f
Sweezer <61049494+Sweesse@users.noreply.github.com>
2020-03-03 11:20:07 +0100

    Upload Snake.ml

--- Snake.ml
+++ Snake.ml
@@ -1,5 +1,4 @@
-#use "CPutil_sn.ml";;
-
+#use "CPinter_sn.ml";;
 let mytranslation_x() : int =
   100;;
 
@@ -21,6 +20,7 @@ let mygraphic_y(y : int ) : int =
 (* Type et fonction de base *)
 
 type t_point = {x : int ; y : int};;
+type t_direction = UP | DOWN | RIGHT | LEFT;;
 type t_position = {pt : t_point ; dir : t_direction};;
 type t_value = EMPTY | SNAKE | FRAME | PROBLEM ;;
 
@@ -37,9 +37,32 @@ let fonction color_of_value(x : t_value ) : t_color =
 
 
 
-  
+let mymatrix_dx() : int =
+  500;;
+
+let mymatrix_dy() : int =
+  500;;
+
 
 type t_matrix = t_value matrix;;
 type t_snake = t_position list;;
-type t_play = {dt = float ref; sn : t_make ref; mat : t_matrix};;
+type t_play = {dt : float ref; sn : t_snake ref; mat : t_matrix};;
+
+let mydt_init() : float =
+  0.00;;
+
+let mydt_acc() : float =
+
+;;
+
+let mydt_ratio() : float =
+
+;;
+
+let mysnake_length_init() : int =
+3
+;;
+
+let mysnake_position_init () : t_point =
+  {x = 200; y = 200};;
 
