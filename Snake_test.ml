31570211fda56f1be95285306e731f0c1a54e3bc
Sweezer <61049494+Sweesse@users.noreply.github.com>
2020-03-27 17:56:12 +0100

    Add files via upload

--- Snake_test.ml
+++ Snake_test.ml
@@ -1,13 +1,16 @@
 #use "CPtest_sn.ml";;
 #use "Snake.ml";;
 
-(** Nous verifions ici si la valeur de sortie correspond bien a la valeur calculee *)
+(** Test structurel de mygraphic_x
+    @author Duc.
+    @since 1.0
+ *)
 let test_mygraphic_x_functionnal() : unit =
   let test_status : t_test_status  = create_test_status("mygraphic_x") in
   (
-    test_func_equals_value(test_status,"La valeur corresponds bien",mygraphic_x,(5),125);
-    test_func_equals_value(test_status,"Les valeurs corresponds bien",mygraphic_x,(0),100);
-    test_func_equals_value(test_status,"Les valeurs corresponds bien",mygraphic_x,(99),195);
+    test_func_equals_value(test_status,"La valeur 0 corresponds bien",mygraphic_x,(5),125);
+    test_func_equals_value(test_status,"La valeurs 0 corresponds bien",mygraphic_x,(0),100);
+    test_func_equals_value(test_status,"La valeurs 0 corresponds bien",mygraphic_x,(99),195);
     print_test_status(test_status);
   )
 ;;
@@ -15,9 +18,9 @@ test_mygraphic_x_functionnal();;
 let test_mygraphic_y_functionnal() : unit =
   let test_status : t_test_status  = create_test_status("mygraphic_x") in
   (
-    test_func_equals_value(test_status,"La valeur corresponds bien",mygraphic_y,(5),125);
-    test_func_equals_value(test_status,"Les valeurs corresponds bien",mygraphic_y,(0),100);
-    test_func_equals_value(test_status,"Les valeurs corresponds bien",mygraphic_y,(99),195);
+    test_func_equals_value(test_status,"La valeur 5 corresponds bien",mygraphic_y,(5),125);
+    test_func_equals_value(test_status,"La valeurs 0 corresponds bien",mygraphic_y,(0),100);
+    test_func_equals_value(test_status,"La valeurs 99 corresponds bien",mygraphic_y,(99),195);
     print_test_status(test_status);
   )
 ;;
@@ -25,7 +28,7 @@ let test_mygraphic_y_functionnal() : unit =
 test_mygraphic_y_functionnal();;
 (** Selon la valeur du type value, on vérifie qu'elle corresponde bien à la couleur demandée *)
 
-let test_color_of_value_structurel() : unit =
+let test_color_of_value_structural() : unit =
   let test_status : t_test_status = create_test_status("color_of_value") in
   (
     test_func_equals_value(test_status,"La couleur est blanche",color_of_value,(EMPTY),Graphics.white);
@@ -50,3 +53,17 @@ let test_compute_new_position_functionnal() : unit =
 ;;
 
 test_compute_new_position_functionnal();;
+
+
+let test_compute_move_functionnal() : unit =
+  let test_status : t_test_status = create_test_status("compute_new_position") in
+  (
+    test_func_equals_value(test_status,"",compute_move,({pt = { x = 0 ; y = 0};dir = LEFT},LEFT,init_matrix()),({pt = { x = -1 ; y = 0};dir = LEFT},FRAME));
+    test_func_equals_value(test_status,"",compute_move,({pt = { x = 100 ; y = 100};dir = UP},UP,init_matrix()),({pt = { x = 100 ; y = 101};dir = UP},FRAME));
+    print_test_status(test_status);
+  )
+;;
+
+test_compute_move_functionnal();;
+
+                                                      
